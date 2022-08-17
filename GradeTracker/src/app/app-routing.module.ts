e9be28796d7b369ddd2d1184571ed27bc61f157b
ApisanaSenthilrajan <apisana_senthilrajan@sluz.ch>
2022-08-17 13:32:03 +0200

    routing done

--- GradeTracker/src/app/app-routing.module.ts
+++ GradeTracker/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import { SubjectModuleOverviewComponent } from './subject-module-overview/subject-module-overview.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GoalOverviewComponent } from './goal-overview/goal-overview.component';
+import { GradeOverviewComponent } from './grade-overview/grade-overview.component';
+
+const routes: Routes = [
+  
+  {path: `subjectoverview`, component: SubjectModuleOverviewComponent},
+  {path:`registration`, component:RegistrationComponent},
+  {path:`login`, component:LoginComponent},
+  {path:`dashboard`, component:DashboardComponent},
+  {path:`goaloverview`, component:GoalOverviewComponent},
+  {path:`gradeoverview`, component:GradeOverviewComponent},
+  {path: '',   redirectTo: '/registration', pathMatch: 'full' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
