aa026f3e245bfebdac7c6d5ae6c3d032d15168d1
unknown <evan_mueller@sluz.ch>
2022-08-17 11:03:45 +0200

    Add all needed components

--- GradeTracker/src/app/app.module.ts
+++ GradeTracker/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GoalOverviewComponent } from './goal-overview/goal-overview.component';
+import { GradeOverviewComponent } from './grade-overview/grade-overview.component';
+import { SubjectModuleOverviewComponent } from './subject-module-overview/subject-module-overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    DashboardComponent,
+    GoalOverviewComponent,
+    GradeOverviewComponent,
+    SubjectModuleOverviewComponent
   ],
   imports: [
     BrowserModule,
