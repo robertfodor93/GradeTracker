05ba91a6e7b0f2d674bce26c2881d6907aec2caf
ApisanaSenthilrajan <apisana_senthilrajan@sluz.ch>
2022-08-26 10:51:31 +0200

    Creating Form Subject & Exam

---
+++ GradeTracker/src/app/new-subject-module/new-subject-module.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit,Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { Subject } from '../services/module.service';
+
+@Component({
+  selector: 'app-new-subject-module',
+  templateUrl: './new-subject-module.component.html',
+  styleUrls: ['./new-subject-module.component.scss']
+})
+export class NewSubjectModuleComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<NewSubjectModuleComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Subject,
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  
+
+  ngOnInit(): void {
+  }
+
+}
