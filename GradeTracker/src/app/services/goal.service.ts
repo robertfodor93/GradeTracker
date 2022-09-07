1852786e827e64e049cb54fd2f8cde19e2387b82
eweennx <evan_mueller@sluz.ch>
2022-09-07 10:33:55 +0200

    NewGoal Evan

--- GradeTracker/src/app/services/goal.service.ts
+++ GradeTracker/src/app/services/goal.service.ts
@@ -3,11 +3,8 @@ import { Injectable } from '@angular/core';
 
 
 export interface Goal {
-  Id: number;
   name: string;
-  teacher: string;
   goal: number;
-  needed: number;
 }
 
 @Injectable({
@@ -18,15 +15,21 @@ export class GoalService {
 
   private urlGoals = 'https://localhost:7290/api/module/getAll';
   private urlAddGoal = 'https://localhost:7290/api/module/update?id=';
-  
-   
+
+
   constructor(private http: HttpClient) { }
-  
-  getGoals(){
+
+  getGoals() {
     return this.http.get(this.urlGoals);
   }
 
-  setGoals(id:number){
-    return this.http.get(this.urlAddGoal + id);
+  setGoals(dataGoal: Goal) {
+    const headers = { 'content-type': 'application/json' }
+    const goal = JSON.stringify(dataGoal);
+    console.log(goal)
+    this.http.post('https://localhost:7290/api/module/update?id=' + 1 , goal, { 'headers': headers }).subscribe((result) => {
+      console.warn("result", result);
+
+    });
   }
 }
