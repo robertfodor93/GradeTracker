c04fc69796dc962991db378120fbbe465b88f69f
robertfodor93 <robert.fodor@sluz.ch>
2022-09-09 11:48:04 +0200

    Description added to Mark Entity and DTO

--- GradeTrackerAPI/DataTransferObjects/MarkDto.cs
+++ GradeTrackerAPI/DataTransferObjects/MarkDto.cs
@@ -3,6 +3,7 @@
     public class MarkDto
     {
         public double Grade { get; set; }
+        public string Description { get; set; }
         public double Weighting { get; set; }
         public DateTime Date { get; set; }
         public int ModuleId { get; set; }
--- GradeTrackerAPI/Entities/Mark.cs
+++ GradeTrackerAPI/Entities/Mark.cs
@@ -3,6 +3,7 @@
     public class Mark : BaseEntity
     {
         public double? Grade { get; set; }
+        public string? Description { get; set; } = string.Empty;
         public double? Weighting { get; set; }
         public DateTime? Date { get; set; } = DateTime.Now;
         public int? ModuleId { get; set; }
